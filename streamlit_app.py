79b0848e637283bf52ca7a4900807a76f5ef96bf
Valexand80 <152338342+Valexand80@users.noreply.github.com>
2024-08-03 12:09:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('Data analysis and aggregation')
 
-st.info('In this app, we wwill be performing data analysis, data summary and aggregation using the Palmer Penguins dataset')
+st.info('In this app, we will be performing data analysis, data summary and aggregation using the Palmer Penguins dataset')
