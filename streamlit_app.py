2ad9a06ece5db3fc8372af40a4526b896053d255
Valexand80 <152338342+Valexand80@users.noreply.github.com>
2024-08-03 12:41:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,6 @@ st.title('Data analysis and aggregation')
 st.info('In this app, we will be performing data analysis, data summary and aggregation using the Palmer Penguins dataset')
 
 penguins = sns.load_dataset('penguins')
-st.expander('Data')
+with st.expander('Data'):
+  st.write('**Raw data**)
   penguins
