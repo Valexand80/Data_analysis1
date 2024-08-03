ed07617abc63267e277a1d93c2cf334a0e0bc89b
Valexand80 <152338342+Valexand80@users.noreply.github.com>
2024-08-03 12:23:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import seaborn as sns
+import pandas as pd
+import numpy as np
 
 st.title('Data analysis and aggregation')
 
 st.info('In this app, we will be performing data analysis, data summary and aggregation using the Palmer Penguins dataset')
 
+penguins = sns.load_dataset('penguins')
+penguins
